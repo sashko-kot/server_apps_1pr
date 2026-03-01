519c19f65a5a0bd343cb5b2556632deacc1568e9
sashokot <asveatan@yandex.ru>
2026-03-01 19:56:25 +0300

    Выполнено задание 1.3

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class numbers(BaseModel):
+    num1: int
+    num2: int
+
+@app.post("/calculate")
+async def calculate(nums: numbers):
+    result = nums.num1 + nums.num2
+    return {"result": result}
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
