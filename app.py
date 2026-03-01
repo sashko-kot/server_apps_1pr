3beadb975fba9590184d41ff9b86ff8ef32640a7
sashokot <asveatan@yandex.ru>
2026-03-02 01:45:05 +0300

    Выполнено задание 2.2

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ from models import userResponse
 from models import feedbackResponce
 from models import feedback
 from datetime import datetime
+from fastapi import Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
 
 app = FastAPI()
 
 feedbackStorage = []
 feedbackCounter = 0
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={"detail": exc.errors()}
+    )
 
 @app.post("/feedback", response_model=feedbackResponce)
 async def createFeedback(feedback: feedback):
--- models.py
+++ models.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from typing import List, Optional
+from typing import ClassVar
+import re
 
 class User(BaseModel):
     name: str
@@ -10,8 +13,30 @@ class userResponse(BaseModel):
     isAdult: bool
 
 class feedback(BaseModel):
-    name: str
-    message: str
+    name: str = Field(..., min_length=2, max_length=50)
+    message: str = Field(..., min_length=10, max_length=500)
+
+    forbiddenWords: ClassVar[List[str]] = ["кринж", "рофл", "вайб"]
+    @field_validator('message')
+    @classmethod
+    def check_message(cls, v:str):
+        messageLower = v.lower()
+        for word in cls.forbiddenWords:
+            pattern = r'\b' + re.escape(word) + r'\b'
+            if re.search(pattern, messageLower):
+                raise ValueError(f"Использование недопустимых слов")
+        return v
+    
+    @field_validator('name')
+    @classmethod
+    def validate_name(cls, v: str):
+        if not v.strip():
+            raise ValueError("Имя не может быть пустым или состоять только из пробелов")
+        
+        if not re.match(r'^[a-zA-Zа-яА-Я\s-]+$', v):
+            raise ValueError("Имя может содержать только буквы, пробелы и дефисы")
+        
+        return v.strip()
 
 class feedbackResponce(BaseModel):
     message: str
